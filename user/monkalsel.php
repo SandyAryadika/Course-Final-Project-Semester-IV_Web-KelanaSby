6cbb5efce6a83a6959677b858e3c80e2e76d5e72
unknown <aryadikawidodo0@gmail.com>
2024-06-03 09:10:46 +0700

    update monkasel

---
+++ user/monkalsel.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugu Pahlawan</title>
+    <link rel="stylesheet" href="../css/monkalsel-style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav aria-label="Main Navigation">
+        <a href="../user/home.php" class="logo">KelanaSby</a>
+            <ul>
+                <li><a href="#aboutus">Home</a></li>
+                <li><a href="#gallery" a href="gallery">Gallery</a></li>
+                <li><a href="fasilitas">Fasilitas</a></li>
+                <li><a href="../user/home.php" a href="#contact">Contact Us</a></li>
+            </ul>
+            <div class="auth-buttons">
+            <button class="login" onclick="window.location.href='../user/loginRegist.php'">Login</button>
+            <button class="signup"onclick="window.location.href='../user/loginRegist.php'">Sign Up</button>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section class="hero">
+            <!-- <div class="hero-text">
+                <h1>Tugu Pahlawan</h1>
+                <h2>Warisan Keberanian Nasional.</h2>
+                <div class="hero-buttons">
+                    <button class="get-started"><a href="#aboutus">Mulai</a></button>
+                </div>
+            </div> -->
+        </section>
+
+        <section id="aboutus">
+            <div class="container">
+                <div class="container-content">
+                    <div class="grid-3">
+                        <h4><span>Monumen Kapal Selam</span>&nbsp;&nbsp;&nbsp;&nbsp; Surabaya, Jawa Timur</h4><br> 
+                    </div>
+                    <p>Monkasel merupakan monumen skala penuh dari KRI Pasopati 410. Kapal selam ini merupakan salah satu Armada Divisi Timur. Basofi Soedirman, menjadi peletak batu pertama dalam konstruksi monumen yang dimulai pada Juli 1995.
+                        Meskipun monumen ini bukanlah replika, namun pembuatannya dilakukan dengan membagi kapal menjadi 16 bagian. KRI Pasopati 410 diiris di PT PAL Indonesia, yang kemudian bagiannya disambung kembali. Pada 15 Juli 1998, Monkasel resmi buka dan menjadi objek wisata Surabaya.
+                        Monumen berwarna hijau ini merupakan monumen kapal selam terbesar di Asia. Pembangunannya berasal dari ide para sesepuh kapal selam dan angkatan laut. Selain sebagai wisata, Monkasel juga berdiri sebagai warisan sejarah Indonesia sebagai Negara Maritim.</p>
+                        <div class="grid-3">
+                        <h5>Arsitektur</h5>
+                        <p>Monkasel terdiri dari kapal selam KRI Pasopati 410 yang ditempatkan di darat dan dimodifikasi menjadi museum. Di dalamnya, pengunjung dapat melihat berbagai ruangan dan peralatan yang digunakan oleh awak kapal selam, termasuk ruang kendali, ruang tidur, dan ruang mesin. Kapal ini dipertahankan dalam kondisi aslinya, memberikan pengalaman yang autentik tentang kehidupan di dalam kapal selam.</p>
+                    </div>
+                    <div class="grid-3">
+                        <h5>Lokasi</h5>
+                        <p>Monkasel terletak di Jalan Pemuda No. 39, Surabaya, Jawa Timur. Lokasinya strategis dan mudah dijangkau dari berbagai bagian kota.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <hr>
+
+        <article id="gallery">
+            <div class="gallery">
+                <div class="gallery-content">
+                    <div class="grid-3">
+                        <h4><span>Gallery</span>&nbsp;&nbsp;&nbsp;&nbsp; House of Sampoerna</h4><br> 
+                    </div>
+                    <p>Nikmati koleksi foto Monkasel dari berbagai sudut dan momen bersejarah.</p>
+                </div>
+            </div>
+            <section id="card">
+            <div class="card-header">
+                <div class="card-container">
+                    <div class="card">
+                        <img src="../Assets/Gallery_monkasel .jpg">
+                    </div>
+                    <div class="card">
+                        <img src="../Assets/Gallery_monkasel 2.jpg">
+                    </div>
+                    <div class="card">
+                        <img src="../Assets/Gallery_monkasel 3.jpg">
+                    </div>
+                </div>
+            </div>
+        </article>
+
+        <article id>
+        <div class="fasilitas">
+                <div class="fasilitas-content">
+                    <div class="grid-3">
+                        <h4><span>Fasilitas</span>&nbsp;&nbsp;&nbsp;&nbsp; House of Sampoerna</h4><br> 
+                    </div>
+                    <p>Monumen Kapal Selam (Monkasel) menyediakan berbagai fasilitas untuk kenyamanan pengunjung, termasuk area parkir luas untuk kendaraan roda dua dan empat, toilet bersih, dan kios makanan serta minuman. Pengunjung juga dapat menikmati taman yang rimbun, area rekreasi, dan spot foto menarik. Ruang pameran menampilkan artefak sejarah kapal selam KRI Pasopati 410, sementara ruang edukasi dan workshop mendukung program pendidikan. Toko suvenir menawarkan berbagai merchandise khas Monkasel, dan pusat informasi menyediakan brosur serta peta. Layanan pemandu tur juga tersedia untuk memberikan penjelasan mendetail tentang sejarah dan fitur kapal selam.</p>
+                    <div class="grid-3">
+                        <h5>Jam Buka</h5>
+                        <p>Monkasel buka setiap hari dari pukul 08:00 hingga 21:00. Pengunjung disarankan untuk datang lebih awal atau di sore hari untuk menghindari keramaian dan menikmati suasana yang lebih tenang.</p>
+                    </div>
+
+                    <div class="grid-3">
+                        <h5>Harga Tiket</h5>
+                        <p>Retribusi	Tarif
+                            Tiket Masuk	Gratis<br>
+                            Dewasa: Rp 15.000<br>
+                            Anak-anak: Rp 10.000<br>
+                    </div>
+                </div>
+            </div>
+        </article>
+    </main>
+
+    <footer>
+    <section id="foot">
+            <div class="footer">
+                <p>Pemrograman Web A081 - Kelompok 11</p>
+            </div>
+        </section>
+    </footer>
+</body>
+</html>
